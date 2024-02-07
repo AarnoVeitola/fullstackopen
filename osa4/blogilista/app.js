a4f2934e46605c4707c3239b1d532c34164943ba
AarnoVeitola <121627348+AarnoVeitola@users.noreply.github.com>
2024-02-07 21:45:57 +0200

    added exercises 4.1.-4.2.

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,37 @@
+const config = require('./utils/config')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use('/', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
