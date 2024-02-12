efd3d07677b660eb77e449dc1581fce578f5ee6b
AarnoVeitola <121627348+AarnoVeitola@users.noreply.github.com>
2024-02-12 16:46:39 +0200

    added 4.14-4.15

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,13 +1,18 @@
 const config = require('./utils/config')
 const express = require('express')
-require('express-async-errors')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
+
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+require('express-async-errors')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+const middleware = require('./utils/middleware')
 
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
@@ -31,6 +36,8 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
